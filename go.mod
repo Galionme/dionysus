3f2a9881d100baaf601ec1a9580045c089225bf9
Galionme <maksbold2022@gmail.com>
2024-04-13 19:42:38 +0700

    init golang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Galionme/dionysus
+
+go 1.22.2
--- util/reader.go
+++ util/reader.go
@@ -1,1 +1,23 @@
 package util
+
+import (
+	"bufio"
+	"bytes"
+	"os"
+)
+
+func PreProcessImages(dat *os.File) (*bytes.Reader, error) {
+	stat, err := dat.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	var size = stat.Size()
+	b := make([]byte, size)
+
+	bufR := bufio.NewReader(dat)
+	_, err = bufR.Read(b)
+	bReader := bytes.NewReader(b)
+
+	return bReader, err
+}
